9c7b6edfb466f0b9f371a293d1a59954ad2127b0
nikki <6318495+echrs@users.noreply.github.com>
2022-07-16 12:02:33 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ router.route('/login').post(async (req, res) => {
     const existingUser = await User.findOne({ email });
     if (!existingUser) return res.status(404).json({ message: "User doesn't exist" });
     const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
-    if (!isPasswordCorrect) return res.status(400).json({ message: 'Invalid credentials' });
+    if (!isPasswordCorrect) return res.status(401).json({ message: 'Invalid credentials' });
     const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, secret);
     res.status(200).json({ message: 'Successful login', data: { userId: existingUser._id, email: existingUser.email, token: token } });
   } catch (err) {
@@ -32,7 +32,7 @@ router.route('/register').post(async (req, res) => {
       password: hashedPassword,
     });
     const token = jwt.sign({ email: result.email, id: result._id }, secret);
-    res.status(200).json({ message: 'Successful registration', data: { userId: result._id, name: result.name, email: result.email, token: token } });
+    res.status(201).json({ message: 'Successful registration', data: { userId: result._id, name: result.name, email: result.email, token: token } });
   } catch (error) {
     res.status(500).json({ message: 'Something went wrong', data: { error: error } });
   }
