e3c82c0a7a027a9cf01bc677147a4cb0a31589d6
nikki <6318495+echrs@users.noreply.github.com>
2022-07-16 12:18:48 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -10,13 +10,13 @@ router.route('/login').post(async (req, res) => {
   const { email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email });
-    if (!existingUser) return res.status(404).json({ message: "User doesn't exist" });
+    if (!existingUser) return res.status(404).send("User doesn't exist");
     const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
-    if (!isPasswordCorrect) return res.status(401).json({ message: 'Invalid credentials' });
+    if (!isPasswordCorrect) return res.status(401).send('Invalid credentials');
     const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, secret);
-    res.status(200).json({ message: 'Successful login', data: { userId: existingUser._id, email: existingUser.email, token: token } });
+    res.status(200).json({ userId: existingUser._id, name: existingUser.name, email: existingUser.email, token: token });
   } catch (err) {
-    res.status(500).json({ message: 'Something went wrong', data: { error: error } });
+    res.status(500).send('Something went wrong');
   }
 });
 
@@ -24,7 +24,7 @@ router.route('/register').post(async (req, res) => {
   const { name, email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(400).json({ message: 'User already exists' });
+    if (existingUser) return res.status(400).send('User already exists');
     const hashedPassword = await bcrypt.hash(password, 12);
     const result = await User.create({
       name,
@@ -32,9 +32,9 @@ router.route('/register').post(async (req, res) => {
       password: hashedPassword,
     });
     const token = jwt.sign({ email: result.email, id: result._id }, secret);
-    res.status(201).json({ message: 'Successful registration', data: { userId: result._id, name: result.name, email: result.email, token: token } });
+    res.status(201).json({ userId: result._id, name: result.name, email: result.email, token: token });
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong', data: { error: error } });
+    res.status(500).send('Something went wrong');
   }
 });
 
