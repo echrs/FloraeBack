975589452d3670556a3b7cff95607eb544735d11
nikki <6318495+echrs@users.noreply.github.com>
2022-07-17 17:17:06 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,7 +4,6 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  isGoogleAuth: { type: Boolean },
   plants: { type: Array },
 });
 
