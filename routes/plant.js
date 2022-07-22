2fdeb48acf94ee565d4419a203c9e04a406b19ce
nikki <6318495+echrs@users.noreply.github.com>
2022-07-22 14:05:06 +0200

    Update plant.js

--- routes/plant.js
+++ routes/plant.js
@@ -3,9 +3,9 @@ let Plant = require('../models/plant');
 let User = require('../models/user');
 
 router.route('/').post(async (req, res) => {
-  const { userId, nickname, name, notes, tasks } = req.body;
+  const { userId, nickname, name, notes, tasks, img } = req.body;
   try {
-    const result = await Plant.create({ nickname, name, notes, tasks });
+    const result = await Plant.create({ nickname, name, notes, tasks, img });
     await User.findOneAndUpdate({ _id: userId }, { $push: { plants: result._id } });
     res.status(200).json(result);
   } catch (error) {
