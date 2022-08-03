63596dc4166aeec910325280a3c832356109e571
nikki <6318495+echrs@users.noreply.github.com>
2022-08-03 18:34:03 +0200

    Update plant.js

--- models/plant.js
+++ models/plant.js
@@ -4,9 +4,16 @@ const plantSchema = new Schema({
   nickname: { type: String, required: true },
   name: { type: String },
   notes: { type: String },
-  tasks: { type: Array },
+  tasks: [taskSchema],
   img: { type: String },
-  userId: { type: mongoose.Schema.ObjectId }
+  userId: { type: mongoose.Schema.ObjectId },
+});
+
+const taskSchema = new Schema({
+  name: { type: String, required: true },
+  repeatDays: { type: Number, required: true },
+  time: { type: Number },
+  taskDate: { type: String, required: true },
 });
 
 const Plant = mongoose.model('Plant', plantSchema, 'plants');
